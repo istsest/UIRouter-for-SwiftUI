83f6a024560dec8001e56b37eaf5036dbd9b47e8
Joon <istsest@hotmail.com>
2025-12-08 15:01:28 -0800

    wip

---
+++ UIRouterTest/UIRouterTest/UIRouterTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIRouterTestApp.swift
+//  UIRouterTest
+//
+//  Created by Joon Jang on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct UIRouterTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
